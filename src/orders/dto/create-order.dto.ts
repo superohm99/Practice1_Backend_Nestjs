b1fb248e39806e83e50b431c1e9a4b63c0d68f4a
T'Ohm <124668047+superohm99@users.noreply.github.com>
2023-12-24 00:26:10 +0700

    Add files via upload

---
+++ src/orders/dto/create-order.dto.ts
@@ -0,0 +1,16 @@
+import { Type } from "class-transformer";
+import { CreateShippingDto } from "./create-shipping.dto";
+import { ValidateNested } from "class-validator";
+import { OrderedProductsDto } from "./ordered-products.dto";
+
+export class CreateOrderDto {
+    @Type(()=>CreateShippingDto)
+    @ValidateNested()
+    shippingAddress:CreateShippingDto;
+
+    @Type(()=> OrderedProductsDto)
+    @ValidateNested()
+    orderedProducts:OrderedProductsDto[];
+
+    
+}
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,39 @@
+import { Roles } from "src/utility/common/user-roles.enum";
+import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, Timestamp, UpdateDateColumn } from "typeorm";
+import { CategoryEntity } from "src/categories/entities/category.entity";
+import { ProductEntity } from "src/products/entities/product.entity";
+import { ReviewEntity } from "src/reviews/entities/review.entity";
+import { OrderEntity } from "src/orders/entities/order.entity";
+
+@Entity('users')
+export class UserEntity {
+@PrimaryGeneratedColumn()
+id:number;
+@Column()
+name:string;
+@Column({unique:true})
+email:string;
+@Column({select:false})
+password:string;
+@Column({type:'enum',enum:Roles,array:true,default:[Roles.USER]})
+roles:Roles[];
+@CreateDateColumn()
+createdAt:Timestamp;
+@UpdateDateColumn()
+updateAt:Timestamp;
+
+@OneToMany(()=>CategoryEntity,(cat)=>cat.addedBy)
+categories:CategoryEntity[];
+
+@OneToMany(()=>ProductEntity,(prod)=>prod.category)
+products:ProductEntity[]
+
+@OneToMany(()=>ReviewEntity,(rev)=>rev.user)
+reviews:ReviewEntity[]
+
+@OneToMany(()=>OrderEntity,(order)=>order.updatedBy)
+ordersUpdateBy:OrderEntity
+
+@OneToMany(()=>OrderEntity,(order)=>order.user)
+orders:OrderEntity[];
+}
